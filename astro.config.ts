1e6b6b50d7908080a0ef8cbb02308f386c2cb502
yaoduren123 <chinaren1314@gmail.com>
2025-04-20 12:23:51 +0800

    chore: 添加多张图片资源并更新依赖项
    新增多张图片资源至 `public/assets` 目录，包括 `多智能体.png`、`2_diagram.png` 等。同时更新 `package.json` 添加 `flowbite` 依赖，并调整 `astro.config.ts` 以支持 MDX 文件。此外，更新了 `src/styles/global.css` 和 `src/content.config.ts` 以优化样式和内容加载逻辑。

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import mdx from '@astrojs/mdx';
 import sitemap from "@astrojs/sitemap";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
@@ -11,6 +12,13 @@ export default defineConfig({
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
+    }),
+    mdx({
+      syntaxHighlight: 'shiki', // 或 'shiki'
+      remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of Contents" }]], // 添加 remark 插件
+      rehypePlugins: [], // 添加 rehype 插件
+      remarkRehype: {}, // remark-rehype 选项
+      gfm: true, // 启用 GitHub Flavored Markdown
     }),
   ],
   markdown: {
