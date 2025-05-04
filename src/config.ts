240f6cf588dc74bc554edc77d1a20ad72d3e5e07
yaoduren123 <chinaren1314@gmail.com>
2025-05-04 19:08:21 +0800

    feat: 添加设计指南文章并更新相关配置
    添加了《写给开发者的设计指南（三）—— 组件篇》文章，包含组件设计的基本规则、色彩、排版等内容。同时更新了配置文件中的语言设置，并调整了DosDonts组件的高度样式。

--- src/config.ts
+++ src/config.ts
@@ -17,7 +17,7 @@ export const SITE = {
     url: "https://github.com/satnaing/astro-paper/edit/main/",
   },
   dynamicOgImage: true,
-  lang: "zh-CN", // html lang code. Set this empty and default will be "en"
+  lang: "en", // html lang code. Set this empty and default will be "en"
   timezone: "Asia/Shanghai", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
 
